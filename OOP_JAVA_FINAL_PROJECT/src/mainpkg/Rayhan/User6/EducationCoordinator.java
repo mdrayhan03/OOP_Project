2fbc5c8cbadd20bc59d5ace11df5306b07cfb042
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 01:49:51 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User6/EducationCoordinator.java
@@ -0,0 +1,39 @@
+package mainpkg.Rayhan.User6;
+
+import mainpkg.AbstractClass.User;
+
+/**
+ *
+ * @author RayhaN
+ */
+public class EducationCoordinator extends User {
+    int teacherAmount = 0 ;
+    int studentAmount = 0 ;
+
+    public EducationCoordinator(int id, String name, String password, String phoneNo, String email, String userType, String dob) {
+        super(id, name, password, phoneNo, email, userType, dob);
+    }
+
+    public int getTeacherAmount() {
+        return teacherAmount;
+    }
+
+    public int getStudentAmount() {
+        return studentAmount;
+    }
+
+    public void setTeacherAmount(int teacherAmount) {
+        this.teacherAmount += teacherAmount;
+    }
+
+    public void setStudentAmount(int studentAmount) {
+        this.studentAmount += studentAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "EducationCoordinator{" + "teacherAmount=" + teacherAmount + ", studentAmount=" + studentAmount + '}';
+    }
+    
+    
+}

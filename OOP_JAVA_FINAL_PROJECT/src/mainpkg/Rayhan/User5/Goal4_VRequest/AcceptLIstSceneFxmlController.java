31dfb5bed5e0883f16ac09e9813a1cf4a114f706
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 01:02:04 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal4_VRequest/AcceptLIstSceneFxmlController.java
@@ -0,0 +1,26 @@
+package mainpkg.Rayhan.User5.Goal4_VRequest;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author RayhaN
+ */
+public class AcceptLIstSceneFxmlController implements Initializable {
+
+    @FXML    private TableView<?> acceptListTableView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

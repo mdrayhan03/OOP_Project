9510ef12666010a0ba77e1b3c631a67c597f8234
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 02:14:39 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal5_Schedule/ScheduleSceneFxmlController.java
@@ -0,0 +1,49 @@
+package mainpkg.Rayhan.User5.Goal5_Schedule;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author RayhaN
+ */
+public class ScheduleSceneFxmlController implements Initializable {
+
+    @FXML
+    private TableView<?> scheduleTableView;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backOnMouseClick(MouseEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/mainpkg/Rayhan/User5/DashBoardSceneFxml.fxml")) ;
+        root = (Parent) myLoader.load() ;
+        Scene myScene = new Scene(root) ;
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+        stage.setScene(myScene) ;
+        stage.setTitle("Volunteer Coordinator DashBoard") ;
+        stage.show() ;
+    }
+    
+}

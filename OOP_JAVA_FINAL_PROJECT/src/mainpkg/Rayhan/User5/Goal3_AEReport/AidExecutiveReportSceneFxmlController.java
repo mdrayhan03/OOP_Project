defabac3a42505d9d17c6657e3426758437ec846
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 01:01:12 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal3_AEReport/AidExecutiveReportSceneFxmlController.java
@@ -0,0 +1,69 @@
+package mainpkg.Rayhan.User5.Goal3_AEReport;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author RayhaN
+ */
+public class AidExecutiveReportSceneFxmlController implements Initializable {
+
+    @FXML    private TextField ddTextField;
+    @FXML    private TextField subjectTextField;
+    @FXML    private TextField mmTextField;
+    @FXML    private TextField yyyyTextField;
+    @FXML    private TextArea reportBodyTextArea;
+    @FXML    private TextField aeIdTextField;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backOnMouseClick(MouseEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/mainpkg/Rayhan/User5/DashBoardSceneFxml.fxml")) ;
+        root = (Parent) myLoader.load() ;
+        Scene myScene = new Scene(root) ;
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+        stage.setScene(myScene) ;
+        stage.setTitle("Volunteer Coordinator DashBoard") ;
+        stage.show() ;
+    }
+
+    @FXML
+    private void submitOnMouseClick(MouseEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/mainpkg/Rayhan/User5/Goal2_SIReport/ShowReportFxml.fxml")) ;
+        root = (Parent) myLoader.load() ;
+        Scene myScene = new Scene(root) ;
+
+        Stage stage = new Stage() ;
+        stage.setScene(myScene) ;
+        stage.getIcons().add(new Image("/image/campIcon.jpg")) ;
+        stage.setTitle("Volunteer Coordinator DashBoard") ;
+        stage.show() ;
+    }
+    
+}

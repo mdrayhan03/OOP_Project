e3c8b1f70f8e9e6e030b8aea0c5bd5d41a04401e
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 01:04:14 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal6_Campaign/CampaignSceneFxmlController.java
@@ -0,0 +1,77 @@
+package mainpkg.Rayhan.User5.Goal6_Campaign;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author RayhaN
+ */
+public class CampaignSceneFxmlController implements Initializable {
+
+    @FXML    private TextField ddTextField;
+    @FXML    private TextField mmTextField;
+    @FXML    private TextField yyyyTextField;
+    @FXML    private ComboBox<?> timeComboBox;
+    @FXML    private ComboBox<?> placeComboBox;
+    @FXML    private TextField reasonTextField;
+    @FXML    private TextArea descriptionTextArea;
+    @FXML    private TableView<?> campaignTableView;
+    @FXML    private TableColumn<?, ?> idTableColumn;
+    @FXML    private TableColumn<?, ?> reasonTableColumn;
+    @FXML    private TableColumn<?, ?> dateTableColumn;
+    @FXML    private TableColumn<?, ?> timeTableColumn;
+    @FXML    private TableColumn<?, ?> descriptionTableColumn;
+    @FXML    private TableColumn<?, ?> statusTableColumn;
+    
+    Alert alert ;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backOnMouseClick(MouseEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/mainpkg/Rayhan/User5/DashBoardSceneFxml.fxml")) ;
+        root = (Parent) myLoader.load() ;
+        Scene myScene = new Scene(root) ;
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+        stage.setScene(myScene) ;
+        stage.setTitle("Volunteer Coordinator DashBoard") ;
+        stage.show() ;
+    }
+
+    @FXML
+    private void applyOnMouseClick(MouseEvent event) {
+        System.out.println("Pressed");
+        alert = new Alert(Alert.AlertType.INFORMATION) ;
+        alert.setHeaderText("Done") ;
+        alert.setContentText("Apply for Campaign is sucessfull.") ;
+        alert.showAndWait() ;
+    }
+    
+}

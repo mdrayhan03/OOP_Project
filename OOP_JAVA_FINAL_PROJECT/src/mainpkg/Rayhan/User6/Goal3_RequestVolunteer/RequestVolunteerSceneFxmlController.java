6639acc4e77d38a69b9ffde6954811afb31a4854
mdrayhan03 <153529870+mdrayhan03@users.noreply.github.com>
2024-03-26 15:19:29 +0600

    Add files via upload

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User6/Goal3_RequestVolunteer/RequestVolunteerSceneFxmlController.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg.Rayhan.User6.Goal3_RequestVolunteer;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author RayhaN
+ */
+public class RequestVolunteerSceneFxmlController implements Initializable {
+
+    @FXML    private TextField reasonTextField;
+    @FXML    private TextArea descriptionTextArea;
+    @FXML    private TextField ddTextField;
+    @FXML    private TextField mmTextField;
+    @FXML    private TextField yyyyTextField;
+    @FXML    private TextField amountTextField;
+    @FXML    private ComboBox<?> timeComboBox;
+    @FXML    private ComboBox<?> placeComboBox;
+    @FXML    private TableView<?> requestTableView;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backOnMouseClick(MouseEvent event) throws IOException {
+        Parent root = null ;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/mainpkg/Rayhan/User6/DashBoardSceneFxml.fxml")) ;
+        root = (Parent) myLoader.load() ;
+        Scene myScene = new Scene(root) ;
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+        stage.setScene(myScene) ;
+        stage.setTitle("Education Coordinator DashBoard") ;
+        stage.show() ;
+    }
+
+    @FXML
+    private void requestOnMouseClick(MouseEvent event) {
+    }
+    
+}

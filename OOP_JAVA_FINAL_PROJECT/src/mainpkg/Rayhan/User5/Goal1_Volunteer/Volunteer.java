806546fa61d97e4996317db0001e488f51c3db88
mdrayhan03 <phylosopherhossain2022@gmail.com>
2024-04-07 09:44:24 +0600

    Commit

--- OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal1_Volunteer/Volunteer.java
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Rayhan/User5/Goal1_Volunteer/Volunteer.java
@@ -1,43 +1,12 @@
 package mainpkg.Rayhan.User5.Goal1_Volunteer;
 
-<<<<<<< HEAD
-=======
 import java.io.Serializable;
 
->>>>>>> Rayhan
 /**
  *
  * @author RayhaN
  */
-<<<<<<< HEAD
-public class Volunteer {
-    String name , pN , id ;
 
-    public Volunteer(String name, String pN) {
-        this.name = name ;
-        this.pN = pN ;
-        this.generateId() ;
-    }
-
-    public String getName() {
-        return name ;
-    }
-
-    public String getpN() {
-        return pN ;
-    }
-
-    public String getId() {
-        return id ;
-    }
-
-    public void setName(String name) {
-        this.name = name ;
-    }
-
-    public void setpN(String pN) {
-        this.pN = pN ;
-=======
 public class Volunteer implements Serializable {
     String id , name , phoneN , addedBy ;
 
@@ -64,17 +33,12 @@ public class Volunteer implements Serializable {
 
     public String getAddedBy() {
         return addedBy;
->>>>>>> Rayhan
+
     }
 
     public void generateId() {
         String id = "Vol" ;
-<<<<<<< HEAD
-        id += this.name.substring(0, 3) ;
-        id += this.pN.length() ;
-        
-        this.id = id ;
-=======
+
         id += name.substring(0) ;
         id += phoneN.substring(9) ;
         id += name.length() ;
@@ -92,16 +56,11 @@ public class Volunteer implements Serializable {
 
     public void setAddedBy(String addedBy) {
         this.addedBy = addedBy;
->>>>>>> Rayhan
     }
 
     @Override
     public String toString() {
-<<<<<<< HEAD
-        return "Volunteer{" + "name=" + name + ", pN=" + pN + ", id=" + id + '}' ;
-=======
         return "Volunteer{" + "id=" + id + ", name=" + name + ", pN=" + phoneN + ", addedBy=" + addedBy + '}';
->>>>>>> Rayhan
     }
     
     

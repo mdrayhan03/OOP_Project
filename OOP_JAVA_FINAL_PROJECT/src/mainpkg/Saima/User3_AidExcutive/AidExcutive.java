461e6cf3e536845753c704341ecc1bf96030a38f
SaimaSinthiya <157502331+SaimaSinthiya@users.noreply.github.com>
2024-04-08 13:44:28 +0600

    AidExcutiveDashBoard

---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Saima/User3_AidExcutive/AidExcutive.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainpkg.Saima.User3_AidExcutive;
+
+/**
+ *
+ * @author HP
+ */
+public class AidExcutive {
+    
+}
---
+++ OOP_JAVA_FINAL_PROJECT/src/mainpkg/Saima/User3_AidExcutive/DashBoardSceneFxmlController.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg.Saima.User3_AidExcutive;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author HP
+ */
+public class DashBoardSceneFxmlController implements Initializable {
+
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label userTypeLabel;
+    @FXML
+    private Label idLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void profileOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void addFoodOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void distributeFoodOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void macroFoodDistributionInfoOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void requestExtraFoodOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void addShelterOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void allocateShelterOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void requestExtraShelterOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void reportOnMouseClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void signOutOnMouseClicked(MouseEvent event) {
+    }
+    
+}
